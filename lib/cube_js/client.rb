76fdc2b5f2e84b578e0816bb1ebc3207e2d23f74
Kemal AKIN <kemal.akin@lab2023.com>
2021-12-04 10:32:35 +0300

    fix: client

--- lib/cube_js/client.rb
+++ lib/cube_js/client.rb
@@ -5,28 +5,24 @@ require 'faraday_middleware'
 
 module CubeJS
   class Client
-    attr_reader :options
+    attr_reader :base_url, :api_token
 
     def initialize(api_token: nil, base_url: nil)
-      # Init options
-      @options = {}
-
-      # Handle option variables
-      @options[:base_url] = base_url || ENV['CUBEJS_BASE_URL']
-      @options[:api_token] = api_token || ENV['CUBEJS_API_TOKEN']
+      @base_url = base_url || ENV['CUBEJS_BASE_URL']
+      @api_token = api_token || ENV['CUBEJS_API_TOKEN']
 
       # Check environment variables
-      raise ArgumentError, 'Base URL Not Provided' if @options[:base_url].nil?
-      raise ArgumentError, 'API Token Not Provided' if @options[:api_token].nil?
+      raise ArgumentError, 'Base URL Not Provided' if @base_url.nil?
+      raise ArgumentError, 'API Token Not Provided' if @api_token.nil?
 
       connection
     end
 
     def connection
-      @connection ||= Faraday.new options[:base_url] do |conn|
+      @connection ||= Faraday.new base_url do |conn|
         conn.request  :json
         conn.response :json
-        conn.headers[:Authorization] = options[:api_token]
+        conn.headers[:Authorization] = api_token
         conn.adapter Faraday.default_adapter
       end
     end
