22ba87da831c15adf7ba0d2c26825c402cf60afa
Kemal AKIN <kemal.akin@lab2023.com>
2021-12-04 10:30:00 +0300

    test: implement test case

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,13 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'cube_js'
 
 require 'minitest/autorun'
+
+module Test
+  module Unit
+    class TestCase < Minitest::Test
+      def self.test(string, &block)
+        define_method("test_#{string}", &block)
+      end
+    end
+  end
+end
